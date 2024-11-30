8e416b650f97c5fdcbf7103b888c5a38b221ae91
tferdous17 <tferdous611@gmail.com>
2024-11-30 16:46:33 -0500

    reorder layout

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <LandingPage />
       <AboutMe />
       <ExperienceSection />
-      <SkillsSection />
       <ProjectsSection />
+      <SkillsSection />
     </>
   );
 }
